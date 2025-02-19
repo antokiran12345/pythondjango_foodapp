10f36138ce4bdf72871ef536ac4c32fc45cea60c
antokiran <antokiran12345@gmail.com>
2025-02-19 17:59:49 +0530

    Antokiran

--- food/views.py
+++ food/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hi")
+
+def item(request):
+    return HttpResponse("<H1>This is an item</H1>")
+
